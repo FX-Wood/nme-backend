4eed8ceed7ec2747930a6dbeea21429eabc4ef5e
FX-Wood <fxwood2@gmail.com>
2019-03-18 15:20:15 -0700

    Adding boilerplate

---
+++ server.js
@@ -0,0 +1,24 @@
+// import expressjs
+const express = require('express');
+const app = express();
+
+// import and configure MongoDB/mongoose
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/nmeBackend', { useNewUrlParser: true })
+const db = mongoose.connection;
+db.once('open', () => {
+    console.log(`[nme-backend] Connected to MongoDB at ${db.host}:${db.port}`)
+})
+db.on('error', (err) => {
+    console.log(`[nme-backend] Database error \n${err}`)
+})
+
+// configure middleware
+app.use(express.urlencoded({ extended: false }))
+app.use('/', (req, res) => {
+    res.send("API located at /api");
+});
+
+
+const port = process.env.PORT || 3001
+app.listen(port, () => console.log('[nme-backend] HTTP server listening on', port))
