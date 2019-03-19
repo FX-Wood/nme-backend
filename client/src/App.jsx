24a60e9ad300e192083467ec702cdfdbd4141163
FX-Wood <fxwood2@gmail.com>
2019-03-19 10:54:59 -0700

    Making proof-of-concept

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,40 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
+  constructor(props) {
+    super(props)
+    this.state = {
+      planets: [],
+      currentPlanet: {},
+      currentSpecies: {},
+      loading: false
+    }
+  }
 
-      </div>
-    );
+  componentDidMount() {
+    axios.get('/api/planets')
+      .then(res => {
+        this.setState({
+          planets: res.data
+        })
+      })
+  }
+
+  render() {
+    let planets
+    if (this.state.planets.length) {
+      planets = this.state.planets.map( (planet, i) => {
+        return <div key={i} ><p>{planet.name}</p> <p>{planet.type}</p></div>
+      })
+    }
+    return (
+    <div className="App">
+      <h1>Hello, here are the planets</h1>
+      {planets}
+    </div>
+    )
   }
 }
-
 export default App;
